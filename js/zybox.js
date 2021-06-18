fdc4cd1ae2583c488c1e8e295e373ed43f9e6e1b
shushu789 <85976805+shushu789@users.noreply.github.com>
2021-06-18 14:09:26 +0800

    Update zybox.js

--- js/zybox.js
+++ js/zybox.js
@@ -10,9 +10,9 @@ const path2 = "/api/tiny-shop/v1/live/room/join";
 
 if(url.indexOf(path1)!=-1){
     let obj = JSON.parse(body);
-    obj.data.level = "8";
-    //obj.data.is_vip = 1;
-    obj.data.is_adm = 1;
+    obj.data.level = "6";
+    obj.data.is_vip = 1;
+    //obj.data.is_adm = 0;
     obj.data.is_open_commission = 1;
     obj.data.current_level = "8";
     //obj.data.pid = "625000";
