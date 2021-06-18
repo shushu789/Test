ae5c86b29b916d2b21ea831afd58521ab4c68b48
shushu789 <85976805+shushu789@users.noreply.github.com>
2021-06-18 12:35:03 +0800

    Update zybox.js

--- js/zybox.js
+++ js/zybox.js
@@ -10,12 +10,12 @@ if(url.indexOf(path1)!=-1){
     let obj = JSON.parse(body);
     obj.data.level = "6";
     obj.data.is_vip = 1;
-    obj.memberLevel.day_long_video = "100"
-    obj.memberLevel.day_short_video = "100"
-    //obj.memberLevel.updated_at = "1601207013"
+    obj.memberLevel.day_long_video = "100";
+    obj.memberLevel.day_short_video = "100";
+    //obj.memberLevel.updated_at = "1601207013";
     obj.memberLevel.day_live_time = "6000";
     obj.memberLevel.level = "6";
-    obj.memberLevel.give_vip_time = "10000"
+    obj.memberLevel.give_vip_time = "10000";
     //obj.vip_time = "2539100750";
     /*
     obj.stats.day_live_time = 6000;
@@ -26,13 +26,13 @@ if(url.indexOf(path1)!=-1){
     obj.data.current_level = "6";
     obj.data.account.level = "6"
     obj.role = "10";
-    obj.memberLevel.day_long_video = "100"
-    obj.memberLevel.day_short_video = "100"
-    //obj.memberLevel.updated_at = "1601207013"
+    obj.memberLevel.day_long_video = "100";
+    obj.memberLevel.day_short_video = "100";
+    //obj.memberLevel.updated_at = "1601207013";
     obj.memberLevel.day_live_time = "6000";
     obj.memberLevel.level = "6";
-    obj.memberLevel.give_vip_time = "10000"
-    //obj.memberLevel.created_at = "1601207013"
+    obj.memberLevel.give_vip_time = "10000";
+    //obj.memberLevel.created_at = "1601207013";
 
 
     obj.pid = "625000";
