53b00145626300a0406beb3b3ff487ddaa374007
shushu789 <85976805+shushu789@users.noreply.github.com>
2021-06-18 13:20:50 +0800

    Update zybox.js

--- js/zybox.js
+++ js/zybox.js
@@ -8,38 +8,38 @@ var url = $request.url;
 const path1 = "/api/tiny-shop/v1/member/member/index";
 if(url.indexOf(path1)!=-1){
     let obj = JSON.parse(body);
-    obj.data.level = "5";
+    obj.data.level = "6";
     obj.data.is_vip = 1;
-    obj.data.current_level = "5";
-    obj.pid = "625000";
-    /*
-    obj.memberLevel.level = "6";
-    obj.memberLevel.give_vip_time = "10000";
-    obj.memberLevel.day_long_video = "100";
-    obj.memberLevel.day_short_video = "100";
-    obj.memberLevel.updated_at = "1601207013";
-    obj.memberLevel.day_live_time = "6000";
+    obj.data.current_level = "6";
+    obj.data.pid = "625000";
+
+    obj.data.memberLevel.level = "6";
+    obj.data.memberLevel.give_vip_time = "10000";
+    obj.data.memberLevel.day_long_video = "100";
+    obj.data.memberLevel.day_short_video = "100";
+    obj.data.memberLevel.updated_at = "1601207013";
+    obj.data.memberLevel.day_live_time = "6000";
 
 
-    obj.vip_time = "2539100750";
+    obj.data.vip_time = "2539100750";
 
-    obj.stats.day_live_time = 6000;
-    obj.stats.day_long_video = 6000;
-    obj.stats.day_short_video = 6000;
+    obj.data.stats.day_live_time = 6000;
+    obj.data.stats.day_long_video = 6000;
+    obj.data.stats.day_short_video = 6000;
 
     obj.data.coupon_num = "10";
     obj.data.current_level = "6";
     obj.data.account.level = "6"
-    obj.role = "10";
-    obj.memberLevel.day_long_video = "100";
-    obj.memberLevel.day_short_video = "100";
-    obj.memberLevel.updated_at = "1601207013";
-    obj.memberLevel.day_live_time = "6000";
-    obj.memberLevel.level = "6";
-    obj.memberLevel.give_vip_time = "10000";
-    obj.memberLevel.created_at = "1601207013";
+    obj.data.role = "10";
+    obj.data.memberLevel.day_long_video = "100";
+    obj.data.memberLevel.day_short_video = "100";
+    obj.data.memberLevel.updated_at = "1601207013";
+    obj.data.memberLevel.day_live_time = "6000";
+    obj.data.memberLevel.level = "6";
+    obj.data.memberLevel.give_vip_time = "10000";
+    obj.data.memberLevel.created_at = "1601207013";
 
-     */
+
     body = JSON.stringify(obj);
 
 }
