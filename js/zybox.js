05174505059ca9039e0bc74c0c01653db3bbe111
shushu789 <85976805+shushu789@users.noreply.github.com>
2021-06-18 12:49:05 +0800

    Update zybox.js

--- js/zybox.js
+++ js/zybox.js
@@ -10,8 +10,10 @@ if(url.indexOf(path1)!=-1){
     let obj = JSON.parse(body);
     obj.data.level = "6";
     obj.data.is_vip = 1;
-    obj.memberLevel.give_vip_time = "10000";
+
+    obj.pid = "625000";
     /*
+    obj.memberLevel.give_vip_time = "10000";
     obj.memberLevel.day_long_video = "100";
     obj.memberLevel.day_short_video = "100";
     obj.memberLevel.updated_at = "1601207013";
@@ -34,10 +36,8 @@ if(url.indexOf(path1)!=-1){
     obj.memberLevel.day_live_time = "6000";
     obj.memberLevel.level = "6";
     obj.memberLevel.give_vip_time = "10000";
-    //obj.memberLevel.created_at = "1601207013";
+    obj.memberLevel.created_at = "1601207013";
 
-
-    obj.pid = "625000";
      */
     body = JSON.stringify(obj);
 
